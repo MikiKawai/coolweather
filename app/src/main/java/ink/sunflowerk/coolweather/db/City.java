9784746841733ec954559cc859a26d6597591178
zhaokui <2441972742@qq.com>
2022-08-22 13:59:22 +0800

    省市县数据

---
+++ app/src/main/java/ink/sunflowerk/coolweather/db/City.java
@@ -0,0 +1,52 @@
+package ink.sunflowerk.coolweather.db;
+
+import org.litepal.crud.LitePalSupport;
+
+/**
+ * @Author zhaokuii11@163.com
+ * @Date 2022-08-22 11:43
+ * @Description 市
+ */
+public class City extends LitePalSupport {
+
+    private int id;
+
+    private String cityName;
+
+    private int cityCode;
+
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+}
---
+++ app/src/main/java/ink/sunflowerk/coolweather/db/Province.java
@@ -0,0 +1,42 @@
+package ink.sunflowerk.coolweather.db;
+
+import org.litepal.crud.LitePalSupport;
+
+/**
+ * @Author zhaokuii11@163.com
+ * @Date 2022-08-22 11:43
+ * @Description 省
+ */
+public class Province extends LitePalSupport {
+
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
+
