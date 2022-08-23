f51b5972cbde0e2ec0ae5cdb36c683bf8314ebe7
zhaokui <2441972742@qq.com>
2022-08-23 09:18:59 +0800

    '加入显示天气信息的功能'

---
+++ app/src/main/java/ink/sunflowerk/coolweather/gson/AQI.java
@@ -0,0 +1,6 @@
+package ink.sunflowerk.coolweather.gson;/**
+@Author zhaokuii11@163.com
+@Date 2022-08-22 14:03
+@Description     
+*/public class AQI {
+}
---
+++ app/src/main/java/ink/sunflowerk/coolweather/gson/Basic.java
@@ -0,0 +1,6 @@
+package ink.sunflowerk.coolweather.gson;/**
+@Author zhaokuii11@163.com
+@Date 2022-08-22 14:00
+@Description     
+*/public class Basic {
+}
