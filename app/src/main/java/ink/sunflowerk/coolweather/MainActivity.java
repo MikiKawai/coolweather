ddc4fd7be3b76f4774c14e86724062e6234c5032
zhaokui <zhaokuii11@163.com>
2022-08-29 09:51:34 +0800

    隐藏程序标题内容

--- app/src/main/java/ink/sunflowerk/coolweather/MainActivity.java
+++ app/src/main/java/ink/sunflowerk/coolweather/MainActivity.java
@@ -2,6 +2,7 @@ package ink.sunflowerk.coolweather;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.preference.PreferenceManager;
@@ -12,6 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //程序启动后上方的一个类型Windows程序标题栏的东西
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide(); //隐藏标题栏
+        }
         SharedPreferences prefs =
                 PreferenceManager.getDefaultSharedPreferences(this);
         if (prefs.getString("weather", null) != null) {
